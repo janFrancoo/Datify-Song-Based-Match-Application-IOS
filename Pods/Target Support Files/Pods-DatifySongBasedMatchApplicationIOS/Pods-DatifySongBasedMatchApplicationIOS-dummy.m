2ef0c4d4694fd848978aa367603c67716f28918f
¨janfrancoo¨ <poisonweeb@gmail.com>
2020-05-23 21:00:38 +0300

    .gitignore fixed

--- Pods/Target Support Files/Pods-DatifySongBasedMatchApplicationIOS/Pods-DatifySongBasedMatchApplicationIOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DatifySongBasedMatchApplicationIOS : NSObject
-@end
-@implementation PodsDummy_Pods_DatifySongBasedMatchApplicationIOS
-@end
